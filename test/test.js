ec7047eb380ab2a6af9c1a149f4f64aeee0c1d5d
Rob Vignerot <robvignerot@gmail.com>
2014-05-21 21:26:00 -0400

    5 of 5 tests passing
    to test install mocha globally 'npm install mocha -g' and run 'mocha
    .\test\test.js -u bdd -R spec' from the roshambattle root

--- test/test.js
+++ test/test.js
@@ -18,13 +18,25 @@ var user = require("d4-user");
 //
 //
 //var userModel = mongoose.model('user', userSchema);
+var existingUser = {
+            name: 'existingUser', 
+            email: 'existingUser@test.com', 
+            password: 'test'
+        },
+    existingUserID;
+user.userModel.create(existingUser, function(err, noob){
+    if (err) throw new Error ('Failed to initialize existingUser');
+    else existingUserID = noob._id;
+    return true;
+});
 
 describe("d4-user", function() {
    describe(".register()", function() {
        it("should throw error when no user given", function(done){
             user.register(undefined, function(err, data){
-                expect(err).to.not.be.undefined;
-                done();
+               expect(err).to.be.ok;
+               expect(data).to.not.be.ok;
+               done();
             });
        });
        it("should throw error when blank password given", function(done){
@@ -34,10 +46,23 @@ describe("d4-user", function() {
                           displayName: 'BigTestUser',
                         createdOn: Date.now()};           
             user.register(ourContent, function(err, data){
-                expect(err).to.not.be.undefined;
-                done();
+               expect(err).to.be.ok;
+               expect(data).to.not.be.ok;
+               done();
             });
-       });   
+       });  
+       it('should throw and error for an existing user email', function(done){
+            ourContent = {authentication:{type:'default',
+                                      details: {emailAddress: 'existingUser@test.com',
+                                      password: 'btu'}},
+                          displayName: 'BigTestUser',
+                        createdOn: Date.now()};           
+           user.register(ourContent, function(err, data){
+               expect(err).to.be.ok;
+               expect(data).to.not.be.ok;
+               done();
+           });
+       });
        it("should create user", function(done){
             ourContent = {authentication:{type:'default',
                                       details: {emailAddress: 'BigTestUser@email.com',
@@ -45,7 +70,7 @@ describe("d4-user", function() {
                           displayName: 'BigTestUser',
                         createdOn: Date.now()};           
             user.register(ourContent, function(err, data){
-                var query = user.userModel.findOne({'email': ourContent.emailAddress});
+                var query = user.userModel.findOne({'email': ourContent.authentication.details.emailAddress});
                 query.exec(function(err, foundUser){
                     expect(err).to.not.be.ok;
                     expect(foundUser).to.be.ok;
@@ -62,7 +87,7 @@ var testTurn = require('d4-roshamturn');
 describe("d4-roshamturn", function() {
     describe('.takeTurn()', function() {
         it('should log return success', function(done){   
-           testTurn.takeTurn({'userid':'5379651cc759b5480b56c8e6', 'weapon':'Rock'}, function(err, success){
+           testTurn.takeTurn({'userid':existingUserID, 'weapon':'Rock'}, function(err, success){
                 if(err){
                     done(err);
                 } 
@@ -72,4 +97,9 @@ describe("d4-roshamturn", function() {
             });
         });
     });
+});
+
+user.userModel.findOneAndRemove({email:'existingUser@test.com'},function(err,res){
+    if (err) throw new Error ('Failed to remove existingUser');
+    return true;
 });
