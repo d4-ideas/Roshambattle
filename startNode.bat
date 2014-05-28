102b8bcfc772782869a3672c30ce1113d0b9a2b5
Rob Vignerot <robvignerot@gmail.com>
2014-05-27 21:36:10 -0400

    Test cleanup and minor fix to d4-user

--- startNode.bat
+++ startNode.bat
@@ -2,8 +2,16 @@ START "runas /user:administrator" cmd /K "cd C:\mongodb\bin\ & mongod"
 
 TIMEOUT 3
 
+START "runas /user:administrator" cmd /K "cd C:\mongodb\bin\ & mongo"
+
+TIMEOUT 3
+
 START "runas /user:administrator" cmd /K "set port=3000 & cd C:\GitHub\roshambattle\ & nodemon app"
 
-timeout 3
+timeout 10
+
+START "runas /user:administrator" cmd /K "cd C:\GitHub\roshambattle\ & mocha .\test\test.js -u bdd -R spec"
+
+TIMEOUT 3
 
 "C:\Program Files (x86)\Brackets\Brackets.exe"
--- test/test.js
+++ test/test.js
@@ -10,29 +10,19 @@ var expect = require("chai").expect;
 var http = require('http');
 
 var user = require("d4-user");
-var existingUser = {
-            name: 'existingUser', 
-            email: 'existingUser@test.com', 
-            password: 'test'
-        },
+var existingUser = {emailAddress: 'BigTestUser@email.com',
+                    password: '123',
+                    displayName: 'BigTestUser'},
     existingUserID;
-user.userModel.create(existingUser, function(err, noob){
-    if (err) throw new Error ('Failed to initialize existingUser');
-    else existingUserID = noob._id;
+
+user.userModel.findOneAndRemove({email:'existingUser@test.com'},function(err,res){
+    console.log('remove');
+    if (err) throw new Error ('Failed to remove existingUser');
+    else console.log('removed');
     return true;
 });
 
-describe("d4-user", function() {
-    describe('.getUser()', function() {
-        it('should return a password for a valid emailAddress', function(done) {
-            user.getUser({email: existingUser.email}, function(err, data){
-                expect(err).to.not.be.ok;
-                expect(data).to.be.ok;
-                done();
-            });
-        });
-    });
-                             
+describe("d4-user", function() {                         
     describe(".register()", function() {
        it("should throw error when no user given", function(done){
             user.register(undefined, function(err, data){
@@ -41,54 +31,57 @@ describe("d4-user", function() {
                done();
             });
        });
-       it("should throw error when blank password given", function(done){
-            var ourContent = {emailAddress: 'BigTestUser@email.com',
-                              password: '',
-                              displayName: 'BigTestUser'};           
-            user.register(ourContent, function(err, data){
+       it("should throw error when blank password given", function(done){       
+            user.register({emailAddress: 'failForNoPasswordUser@email.com',
+                    password: '',
+                    displayName: 'failForNoPasswordUser'}, function(err, data){
                expect(err).to.be.ok;
                expect(data).to.not.be.ok;
                done();
             });
-       });  
-       it('should throw an error for an existing user email', function(done){
-            ourContent = {emailAddress: 'existingUser@test.com',
-                          password: 'btu',
-                          displayName: 'BigTestUser'};           
-           user.register(ourContent, function(err, data){
+       });
+       it("should create user", function(done){        
+            user.register(existingUser, function(err, data){
+                var query = user.userModel.findOne({'email': existingUser.emailAddress});
+                query.exec(function(err, foundUser){
+                    expect(err).to.not.be.ok;
+                    expect(foundUser).to.be.ok;
+                    done();
+                });
+            });
+       });        
+       it('should throw an error for an existing user email', function(done){    
+           user.register(existingUser, function(err, data){
                expect(err).to.be.ok;
                expect(data).to.not.be.ok;
                done();
            });
-       });
-       it("should create user", function(done){
-            ourContent = {emailAddress: 'BigTestUser@email.com',
-                          password: 'btu',
-                          displayName: 'BigTestUser'};           
-            user.register(ourContent, function(err, data){
-                var query = user.userModel.findOne({'email': ourContent.emailAddress});
-                query.exec(function(err, foundUser){
-                    expect(err).to.not.be.ok;
-                    expect(foundUser).to.be.ok;
-                    if(foundUser) foundUser.remove();
-                    done();
-                });
-            });
        });       
    });
+    
+    describe('.getUser()', function() {
+        it('should return a password for a valid emailAddress', function(done) {
+            user.getUser({email: existingUser.emailAddress}, function(err, data){
+                expect(err).to.not.be.ok;
+                expect(data).to.be.ok;
+                done();
+            });
+        });
+    });
+    
     describe('.getUserID()', function(){
         it('should return a userid', function(done){
-            user.getUserID({email:existingUser.email}, function(err, data){
+            user.getUserID({email:existingUser.emailAddress}, function(err, data){
                expect(err).to.not.be.ok;
                expect(data).to.be.ok;
-               //done();                
+               done();                
             });
         });
         it('should not return a userid', function(done){
             user.getUserID({email:'idonotalreadyexist'}, function(err, data){
                expect(err).to.be.ok;
                expect(data).to.not.be.ok;
-               //done();                
+               done();                
             });            
         });
     });
@@ -108,11 +101,4 @@ describe("d4-roshamturn", function() {
             });
         });
     });
-});
-
-user.userModel.findOneAndRemove({email:'existingUser@test.com'},function(err,res){
-    console.log('remove');
-    if (err) throw new Error ('Failed to remove existingUser');
-    else console.log('removed');
-    return true;
 });
