83034bfd6822c1916cff4f7b5d723b03fa105e53
sheetzam <sheetzam@earthlink.net>
2014-05-29 21:09:22 -0400

    now with moar login!

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 /* GET home page. */
 exports.index = function(req, res){
-    if (typeof req.session.username !== 'undefined') {
+    if (typeof req.session.userID !== 'undefined') {
         res.render('index', {title: 'Rochambattle',
                             displayName: req.session.displayName
                             });
     } else {
+		console.log(req.session);
         res.redirect('/login');
     }
 };
