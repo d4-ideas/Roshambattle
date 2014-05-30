1524f006961114d367bb7757214d61a92a0173a0
sheetzam <sheetzam@earthlink.net>
2014-05-29 20:32:05 -0400

    now with sockets and sessions!

--- public/js/main.js
+++ public/js/main.js
@@ -38,9 +38,9 @@ function setTheme(themeIndex){
     }
 }
 
-//if (typeof io !== 'undefined'){
-//	var socket=io.connect('/');
-//}
+if (typeof io !== 'undefined'){
+	var socket=io.connect('/');
+}
 
 $(document).ready(function() {
     var cookieValue = readCookie('theme');
@@ -55,11 +55,11 @@ $(document).ready(function() {
 
     });
 	$('#commit').click(function(){
-		$.post('/selectWeapon',
-			   {weapon : $("[name=weapon]:checked").val()},
-			   function(){}
-		);
-		//socket.emit('Message', {weapon:$("[name=weapon]:checked").val()});
+//		$.post('/selectWeapon',
+//			   {weapon : $("[name=weapon]:checked").val()},
+//			   function(){}
+//		);
+		socket.emit('Message', {weapon:$("[name=weapon]:checked").val()});
 	});
 
     initScanlines();
