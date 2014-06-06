2b0cf8aaeab0770b9037fbe99c33b6b2f6137c86
Rob Vignerot <robvignerot@gmail.com>
2014-06-05 20:19:11 -0400

    minor tweaks

--- routes/processTurn.js
+++ routes/processTurn.js
@@ -2,21 +2,20 @@ var ruser = require('d4-roshamuser');
 var turn = require('d4-roshamturn');
 
 exports.selectWeapon = function (req,res){
-	console.log(req.data);
-	console.log(req.body);
-    console.log(req.session);
-           ruser.setWeapon({'userid':req.session.userID, 'weapon':req.data.weapon}, function(err, success){
+    console.log('enter selectWeapon');
+    if (req.data.weapon){
+    ruser.setWeapon({'userid':req.session.userID, 'weapon':req.data.weapon}, function(err, success){
                 if(err){
-					//we need to return an error event
-                    //res.status(500).json({result:'error', reason:'Bzzz...  try again.  The call to setWeapon failed with reason: '+err.error});
-					req.io.emit('result', {'result':'error', reason:'Bzzz...  try again.  The call to setWeapon failed with reason: '+err.error});
+					req.io.emit('selectWeaponFailure', {reason:'Bzzz...  try again.  The call to setWeapon failed with reason: '+err.error});
                 } 
                 else {
 					console.log ('emitting');
-                    req.io.emit('result',{'result':'ok'});
+                    req.io.emit('selectWeaponSuccess',{'result':'ok'});
                 }
             });
-	console.log(req.session);
+    } else {
+            req.io.emit('selectWeaponFailure', {reason:'Arm yourself'});
+    }
 };
 
 exports.generateTurn = function (req,res){
