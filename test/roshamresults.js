2100ae5139d9f40140feb4cdabf4e28bb5d9142b
Rob Vignerot <robvignerot@gmail.com>
2014-06-12 21:43:25 -0400

    better tests again

--- test/roshamresults.js
+++ test/roshamresults.js
@@ -3,6 +3,16 @@ var expect = require("chai").expect;
 var ruser = require('d4-roshamuser');
 var user = require('d4-user');
 var testResult = require('d4-roshamresult');
+var rockID;
+
+before(function(){
+    user.userModel.findOne({email:'RockUser@email.com'}, function (err, data) {
+        if (err)
+            throw "Couldn't find rock user";
+        else
+            rockID = data._id;
+    });
+});
 
 
 
@@ -23,7 +33,7 @@ describe('d4-roshamresult', function() {
     describe('.getUserTurnResults', function() {
         it('should return the results', function(done){
             var turnDate = Date.now();
-            testResult.getUserTurnResults({turnDate:turnDate, userID:''}, function(err, data) {
+            testResult.getUserTurnResults({turnDate:turnDate, userID:rockID}, function(err, data) {
                 expect(err).to.be.undefined;
                 expect(data).to.be.ok;
                 expect(data.weapon).to.be.string('Rock');
