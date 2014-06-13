7db6c9382550ae62cbea0b6d4cd2558be906a054
Kai Clements <ztm@earthlink.net>
2014-06-13 07:10:09 -0400

    getUserTurnResults almost working now
    and not exactly sure why not

--- test/roshamresults.js
+++ test/roshamresults.js
@@ -4,6 +4,7 @@ var ruser = require('d4-roshamuser');
 var user = require('d4-user');
 var testResult = require('d4-roshamresult');
 var rockID;
+var paperID;
 
 before(function(){
     user.userModel.findOne({email:'RockUser@email.com'}, function (err, data) {
@@ -11,7 +12,28 @@ before(function(){
             throw "Couldn't find rock user";
         else
             rockID = data._id;
+            createResult();
     });
+    user.userModel.findOne({email:'PaperUser@email.com'}, function (err, data) {
+        if (err)
+            throw "Couldn't find paper user";
+        else
+            paperID = data._id;
+            createResult();
+    });
+    
+    function createResult(){
+        if((rockID != undefined)&&(paperID != undefined)){
+            testResult.model.create({
+                    'turnDate': '05/21/2014',
+                    'player1': rockID, 
+                    'weapon1': 'Rock',
+                    'player2': paperID, 
+                    'weapon2': 'Paper',
+                    'winner': 2});
+            
+        }
+    }
 });
 
 
@@ -19,10 +41,9 @@ before(function(){
 describe('d4-roshamresult', function() {
     describe('.getTurnResults', function() {
         it('should return the results', function(done){
-            var turnDate = Date.now();
+            var turnDate = new Date('05/21/2014');
             testResult.getTurnResults({turnDate:turnDate}, 
                                function(err,data){
-                console.log(data);                    
                 expect(err).to.be.undefined;
                 expect(data).to.be.ok;
                 done();
@@ -32,7 +53,7 @@ describe('d4-roshamresult', function() {
     
     describe('.getUserTurnResults', function() {
         it('should return the results', function(done){
-            var turnDate = Date.now();
+            var turnDate = new Date('05/21/2014');
             testResult.getUserTurnResults({turnDate:turnDate, userID:rockID}, function(err, data) {
                 expect(err).to.be.undefined;
                 expect(data).to.be.ok;
