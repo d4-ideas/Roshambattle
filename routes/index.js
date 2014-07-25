0852c36b152b3714a8f2a5ee06b4edb8743baed5
Rob Vignerot <robvignerot@gmail.com>
2014-07-24 20:23:59 -0400

    Sending userID to index.ejs

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@
 exports.index = function(req, res){
     if (typeof req.session.userID !== 'undefined') {
         res.render('index', {title: 'Rochambattle',
-                            displayName: req.session.displayName
+                            displayName: req.session.displayName,
+                            userID: req.session.userID
                             });
     } else {
         res.redirect('/login');
