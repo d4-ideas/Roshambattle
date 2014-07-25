b3b879c85fc706f08b0a1861d394e81466967a36
Rob Vignerot <robvignerot@gmail.com>
2014-07-25 08:18:29 -0400

    minor edits for leaderboard

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -2,12 +2,14 @@ var leader = require('d4-roshamleader');
 
 exports.getLeaderBoard = function(req){
     var input = { 
-                    scoreType: 'winRate',
+                    scoreType: 'totalWins',
                     numScores: 5,
                     startAtRank: 1 
                 };
     
     leader.getScores(input, function(err, data){
+        console.log(err);
+        console.log(data);
         if (err)
             req.io.emit('getLeaderBoardFailure', err.error);
         else
