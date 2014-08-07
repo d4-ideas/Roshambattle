03db7505358b8e313dbf82a78957a042bd9f09ae
Rob Vignerot <robvignerot@gmail.com>
2014-08-06 22:44:07 -0400

    Add update node method

--- test/realmnode.js
+++ test/realmnode.js
@@ -2,7 +2,7 @@ console.log('realmnode');
 var expect = require("chai").expect;
 var node = require('d4-realmnode');
 var user = require('d4-user');
-var rockID;
+var rockID, nodeID;
 
 before(function(done){
     user.userModel.findOne({email:'rockuser@email.com'}, function (err, data) {
@@ -10,9 +10,18 @@ before(function(done){
             throw "Couldn't find rock user";
         else
             rockID = data._id;
-        
-        done();
+
+        node.model.create({user:rockID}, function(err,data){
+            if (err)
+                throw "Failure to create node";
+            else 
+                nodeID = data._id;
+            
+            done();
+        });      
     });
+    
+
 });
 
 after(function(){
@@ -26,6 +35,18 @@ describe('d4-realmnode', function(){
     describe('addNode', function(){
         it('should create a node', function(done){
             node.addNode({user:rockID}, function(err, data){
+                expect(err).to.not.be.ok;
+                expect(data).to.be.ok;
+                done();
+            });
+        });
+    });
+});
+
+describe('d4-realmnode', function(){
+    describe('updateNode', function(){
+        it('should update the node description', function(done){
+            node.updateNode({nodeID: nodeID, description: 'An oasis consisting of a calm pool of water surrounded by lush palm trees.'}, function(err, data){
                 expect(err).to.not.be.ok;
                 expect(data).to.be.ok;
                 done();
