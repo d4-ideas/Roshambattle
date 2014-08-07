293be793707a27de66f43ac2dcf3329c1d5cbbe9
Rob Vignerot <robvignerot@gmail.com>
2014-08-06 21:35:23 -0400

    Re-route

--- routes/index.js
+++ routes/index.js
@@ -11,3 +11,29 @@ exports.index = function(req, res){
         res.redirect('/login');
     }
 };
+
+exports.roshambattle = function(req, res){
+    console.log(req.app.get('job').nextInvocation());
+    if (typeof req.session.userID !== 'undefined') {
+        res.render('roshambattle', {title: 'Rochambattle',
+                            displayName: req.session.displayName,
+                            userID: req.session.userID,
+                            nextTurn: req.app.get('job').nextInvocation()
+                            });
+    } else {
+        res.redirect('/login');
+    }
+};
+
+exports.realms = function(req, res){
+    console.log(req.app.get('job').nextInvocation());
+    if (typeof req.session.userID !== 'undefined') {
+        res.render('realms', {title: 'Rochambattle',
+                            displayName: req.session.displayName,
+                            userID: req.session.userID,
+                            nextTurn: req.app.get('job').nextInvocation()
+                            });
+    } else {
+        res.redirect('/login');
+    }
+};
